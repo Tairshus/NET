7a5cd1805478f33d914556324a4128aff7ac200c
psychopsyduck <nialsande@gmail.com>
2019-05-03 10:22:23 -0300

    Tutorial

---
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Tittle { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string genre { get; set; }
+        public decimal price { get; set; }
+
+    }
+
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
